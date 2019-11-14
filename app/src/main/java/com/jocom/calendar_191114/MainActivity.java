b165c16cf7377c7d147e0c84e8c6615d9ed47add
teha007 <teha007@naver.com>
2019-11-14 15:58:17 +0900

    1번째 커밋

---
+++ app/src/main/java/com/jocom/calendar_191114/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jocom.calendar_191114;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
